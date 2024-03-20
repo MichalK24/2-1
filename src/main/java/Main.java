8c0e1a161dc887edabe5de56b58319ec150ab2a3
MichalK24 <162459522+MichalK24@users.noreply.github.com>
2024-03-20 13:30:27 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+import java.awt.TextArea;
+
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args)
+  {
+    String str = "Hello World";
+    for (int i = 0; i < str.length(); i++)
+      {
+        System.out.println(str.charAt(i));
+      }
+        
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
